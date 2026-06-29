593bd021adb034b8dc91342791756be182455669
Katari Abhiram <abhiramkatariofficial@gmail.com>
2026-06-29 13:46:24 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res = ""
+        for n in s :
+            if n.isalnum():
+                res += n.lower()
+        return res[::-1] == res 
+        

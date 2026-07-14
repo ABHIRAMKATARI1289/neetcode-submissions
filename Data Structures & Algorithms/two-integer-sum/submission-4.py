1d7843b0882a3bd97c1b4307ff47ee7e96c919ba
Katari Abhiram <abhiramkatariofficial@gmail.com>
2026-07-14 10:04:26 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pair = {}
+        for i,num in enumerate(nums):
+            comp = target - num
+            if comp in pair :
+                return [pair[comp],i]
+            pair[num] = i 

fd84e14c70ee07aa832a91f1f0e76f20c6f9c9aa
Katari Abhiram <abhiramkatariofficial@gmail.com>
2026-09-12 13:53:12 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t) :
+            return False
+        h1 = defaultdict(int)
+        h2 = defaultdict(int)
+        for n in s :
+            h1[n] += 1 
+        for n in t :
+            h2[n] += 1 
+
+        for n in s :
+            print(n)
+            if h1[n] != h2[n] :
+                return False 
+        return True 
